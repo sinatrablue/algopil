9321537256c9665f617b577616babae7b129a0de
sinatablue <simonot.robin@gmail.com>
2019-10-14 11:19:13 +0200

    classe atom_type mise de côté ; prblm avec GenAtom identifié

--- data_struct.hpp
+++ data_struct.hpp
@@ -1,45 +1,46 @@
 #include<iostream>
 #include<string>
 
-struct atom_type {
+/* typedef struct atom_type {
 	std::string terminal;
 	std::string nonter;
-};
-// useless comment to test git config
-struct node {
+}atom_type; */
+
+typedef struct node {
 	std::string clas;
 	plus *plus_t;
 	conc *conc_t;
 	fang *fang_t;
 	atom *atom_t;
 	ou *ou_t;
-};
+}node;
 
 typedef struct plus {
 	node *left;
 	node *right;
-};
+}plus;
 
 typedef struct conc {
 	node *left;
 	node *right;
-};
+}conc;
 
 typedef struct fang {
 	node *child;
-};
+}fang;
 
 typedef struct ou {
 	node *left;
 	node *right;
-};
+}ou;
 
 typedef struct atom {
-	atom_type *type;
+	bool *is_term;
 	int *cod;  // 0 ou 1
 	char *action;
 	node *child;
-};
+}atom
+;
 
 // Gen functions :
 
@@ -73,15 +74,21 @@ node *GenOu(node *c){
 	return val;
 }
 
-node *GenAtom(std::string *a, int *code, atom_type *t){
+node *GenAtom(std::string *a, int *code, bool *t){
 	node *val = new node;
 	val->clas="atom";
 	val->atom_t->action=a;
 	val->atom_t->cod=code;
-	val->atom_t->type=t;
+	val->atom_t->is_term=t;
 	return val;
 }
 
+/*  Ce test fonctionne, donc les valeurs qu'on passe à GenAtom dans GenForest ne sont pas du type attendu
+std::string *test;
+int *ttest;
+bool *tttest;
+node *Test = GenAtom(test, ttest, tttest);
+*/
 void GenForet(){
 	int S = 0;
 	int N = 1;
@@ -89,4 +96,5 @@ void GenForet(){
 	int T = 3;
 	int F = 4;
 
-	A[S] = GenConc(GenFang(GenConc(GenConc(GenConc(GenAtom("N",0,"nonter"),GenAtom("->",0,"terminal")),GenAtom("E",0,"nonter")),GenAtom(",",1,"terminal"),GenAtom(";",0,"terminal"))));
+	node A[S] = GenConc(GenFang(GenConc(GenConc(GenConc(GenAtom("N",0,false),GenAtom("->",0,true)),GenAtom("E",0,false)),GenAtom(",",1,true),GenAtom(";",0,true))));
+}

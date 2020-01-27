e626a5424051f5b60b02796dec2c5a31471a0217
sinatrablue <simonot.robin@gmail.com>
2020-01-27 16:32:23 +0100

    Test de scan, GenForet commenté, résultats dans test

--- data_struct.cpp
+++ data_struct.cpp
@@ -5,15 +5,17 @@ using namespace std;
 
 int main(void) {
 
-	int prof=0;
-	GenForet(prof);
-	cout << "Entire forest generated." << endl;
+	//int prof=0;
+	//GenForet(prof);
+	//cout << "Entire forest generated." << endl;
 
 	string phrase = "A='B'+C";
-	string::size_type it_phrase=0;	
+	string::size_type it_phrase=0;
 	string code;
 	int action;
 	char caract;
+	scan(phrase, it_phrase, code, action, caract);
+	cout << it_phrase << " " << code << " " << action << " " << caract << endl;
 
 	return EXIT_SUCCESS;
 }
